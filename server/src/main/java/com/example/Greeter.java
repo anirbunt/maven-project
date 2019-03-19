3b5ac7b3777b247866005cac43c8283342375bab
Anirudh vajjala <anirbunt@gmail.com>
2019-03-19 06:07:53 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
   
   /**
    * @param someone is also necessary
-   * @return this tag is necessary 
+   * @return greeting string 
    */ 
   public  final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
